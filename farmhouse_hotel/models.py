12be4a0795a35bd4c42a351e989bb09e709f7077
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-06-29 04:12:01 +0000

    Move html folder outside template

--- farmhouse_hotel/models.py
+++ farmhouse_hotel/models.py
@@ -1,14 +1,6 @@
 from django.db import models
+import uuid
 
 
-# Create your models here.
-
-class Rooms(models.Model):
-    ROOM_TYPE = (
-        ('SIN', 'single'),
-        ('DLX', 'dulex'),   
-    )
-    #ROOM_STATUS = ()
-    
-    room_type = models.CharField(max_length=10, choices= ROOM_TYPE)
-    #status = models.CharField(choices=ROOM_STATUS, max_length=3)
+class farmhouseModal(models.Model):
+ uid = models.UUIDField(default=uuid.uuid4())
