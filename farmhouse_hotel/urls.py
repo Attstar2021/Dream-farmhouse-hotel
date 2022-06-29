19a607bf72063efd80a0c1aa76a66be159d62c1d
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-06-29 11:35:16 +0000

    Add html and css

--- farmhouse_hotel/urls.py
+++ farmhouse_hotel/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
+from .views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+urlpatterns = [
+    path('', farmhouse_hotel, name='farmhouse'),    
+]
 
+urlpatterns += staticfiles_urlpatterns()
--- farmhouse_hotel/views.py
+++ farmhouse_hotel/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
+def farmhouse_hotel(request):
+    return render(request, 'farmhouse.html')
